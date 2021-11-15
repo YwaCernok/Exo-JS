23838bccb652f4c10795a5844f6d40dc56ac1374
YwaCernok <93765179+YwaCernok@users.noreply.github.com>
2021-11-15 13:22:41 +0400

    Finished exo9

---
+++ Exo 9/assets/js/script.js
@@ -0,0 +1,14 @@
+ var img = document.getElementById("meduse");
+
+ function enlargeImg() {
+    // Set image size to 1.5 times original
+    img.style.transform = "scale(1.1)";
+    // Animation effect 
+    img.style.transition = "transform 0.25s ease";
+  }
+  // Function to reset image size
+  function resetImg() {
+    // Set image size to original
+    img.style.transform = "scale(1)";
+    img.style.transition = "transform 0.25s ease";
+  }
