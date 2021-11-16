e9866e771f4acfd3fc18748fbf42f70766901686
YwaCernok <93765179+YwaCernok@users.noreply.github.com>
2021-11-16 16:33:48 +0400

    Ajouter la corr de Nico

--- Exo 9/assets/js/script.js
+++ Exo 9/assets/js/script.js
@@ -12,3 +12,27 @@
     img.style.transform = "scale(1)";
     img.style.transition = "transform 0.25s ease";
   }
+
+
+/* Correction Nicolas
+//Création de la variable image
+var pika=document.getElementById('pika');
+var initwitdhpika = pika.clientWidth
+
+// création de la fonction click pour intéragir au click
+
+pika.addEventListener('click', function toto (){
+// création d'une variable pour récuperer la largeur de l'image
+var witdhpika = pika.clientWidth;
+// Création d'une condition. Si la largeur de l'image est supérieure a 1500px
+if (witdhpika>1500) {
+// Alors elle reviens à sa taille d'origine
+    pika.style.width = (initwitdhpika+'px')
+    //sinon, elle agrandie sa taille de 10%
+} else {
+    pika.style.width = (witdhpika*1.1+'px')
+}
+});
+
+console.log(initwitdhpika);
+*/  
