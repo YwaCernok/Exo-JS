625a8fd6d7a77f8f626e4cc83f433551098c7eb5
YwaCernok <93765179+YwaCernok@users.noreply.github.com>
2021-11-16 15:03:46 +0400

    Added Correction Nico

--- Exo 8/assets/js/script.js
+++ Exo 8/assets/js/script.js
@@ -7,3 +7,14 @@ function setOldImage()
 {
     document.getElementById("meduse").src ="assets/img/meduse1.jpg";
 }
+
+/* Correction Nicolas
+var pika=document.getElementById('pika');
+
+pika.addEventListener("mouseenter", function change(){
+    pika.src="./assets/img/026.png";
+})
+pika.addEventListener("mouseout", function rechange(){
+    pika.src="./assets/img/025.png";
+})
+*/
